bc8cb7bc687ddff4b0d90cfa36cdbf3e176ca2cf
Raymond Say <say.raymond@gmail.com>
2022-08-26 20:57:06 -0400

    Fixing the roulettebreaker

--- example/roulettebreaker.js
+++ example/roulettebreaker.js
@@ -1,9 +1,143 @@
+import { autoRetry, log } from '/hack/utils.js'
 /** @param {NS} ns */
+let _ns;
 export async function main(ns) {
-	Math.floor = (number) => { return 1 }; Math.random = () => { return 0 };
-	/** use: run this, and then go to roulette and bet on the same choice every time.
-	 *  WARNING!!!
-	 *  the game will crash if you do anything else!
-	 *  save the game and reload to fix.
-	 */
+	/** args[0] == loop, defaults to true, args[1] == tries, default 10, args[2] == number of bets, 0 == unlimited, args[3] == bet, default is maximum of current money up to 100 Mill. */
+	// Save our original functions
+	
+	let floor = Math.floor;
+	let random = Math.random;
+	_ns = ns;
+	// Load our arguments
+	let args = ns.args;
+	let loop = args[0] !== undefined ? args[0]:true;
+	let tries = args[1] !== undefined ? args[1]:10;
+	let loops = args[2] !== undefined ? args[2]:0;
+	let ammount = args[3] !== undefined ? args[3]:100000000;
+	let tail = args[4] !== undefined ? args[4]:false;
+	if( tail ) {
+		ns.tail();
+		await ns.sleep(1000);
+		ns.tprint("Starting Tail")
+	}
+	ns.print(`Loop: ${loop.toString()} Tries: ${tries.toString()} Betting Loops: ${loops.toString()} Bet ammount: ${ammount.toString()}`);
+	let breakMath = function() {
+		//Override default JS functions
+		Math.floor = (number) => { return 1 }; Math.random = () => { return 0 };
+		/** use: run this, and then go to roulette and bet on the same choice every time.
+		 *  WARNING!!!
+		 *  the game will crash if you do anything else!
+		 *  save the game and reload to fix.
+		 */
+	}
+	// fix math function
+	let fixMath = () => {
+		Math.floor = floor;
+		Math.random = random;
+	}
+	// Step 1.1 Setup the run
+	// Helper function to detect if the "Stop [[faction|company] work|styding|training]" etc... button from the focus screen is up
+	const checkForFocusScreen = async () =>
+		await findRetry(ns, "//button[contains(text(), 'Stop playing')]", true) ? false : // False positive, casino "stop" button, no problems here
+			await findRetry(ns, "//button[contains(text(), 'Stop')]", true); // Otherwise, a button with "Stop" on it is probably from the work screen
+	let attempts = 0; 
+	let rtButton, roulButton, wagerField, wheelSlotButton;
+	while (attempts++ <= tries) {
+		if (attempts > 1) 
+			await ns.sleep(1000);
+		try {
+			// Step 2.1: If the player is focused, stop the current action
+			const btnStopAction = await checkForFocusScreen();
+			if (btnStopAction) { // If we were performing an action unfocused, it will be focused on restart and we must stop that action to navigate.
+				log(ns, "It looks like we're on a focus screen. Stopping whatever we're doing...")
+				await click(btnStopAction);
+			}
+			// Step 2.2: Navigate to the City Casino
+			try { // Try to do this without SF4, because it's faster and doesn't require a temp script to be cleaned up below
+				// Click our way to the city casino
+				await click(await findRetry(ns, "//div[(@role = 'button') and (contains(., 'City'))]"));
+				await click(await findRetry(ns, "//span[@aria-label = 'Iker Molina Casino']"));
+			} catch { // Use SF4 as a fallback, it's more reliable.
+				try { await getNsDataThroughFile(ns, 'ns.singularity.goToLocation(ns.args[0])', '/Temp/goToLocation.txt', ["Iker Molina Casino"]); }
+				catch { return tailAndLog(ns, "ERROR: Failed to travel to the casino both using UI navigation and using SF4 as a fall-back."); }
+			}
+			// Step 2.3: Try to start the blackjack game
+			const blackjack = await findRetry(ns, "//button[contains(text(), 'roulette')]");
+			if (!blackjack) {
+				tailAndLog(ns, `ERROR: Could not find the "Play roulette" button. Did something steal focus? Trying again... ` +
+					`Please post a full-game screenshot on Discord if you can't get past this point.`)
+				continue; // Loop back to start and try again
+			}
+			await click(blackjack);
+
+			// Step 2.4: Get some buttons we will need to play blackjack
+			inputWager = await findRetry(ns, "//input[@value = 1000000]");
+			btnStartGame = await findRetry(ns, "//button[text() = '35']");
+			rtButton = await findRetry(ns, "//button[text() = 'Return To World']");
+			if (!inputWager || !btnStartGame || !rtButton) {
+				tailAndLog(ns, `ERROR: Could not find one or more game controls. Did something steal focus? Trying again... ` +
+					`Please post a full-game screenshot on Discord if you can't get past this point.`)
+				continue; // Loop back to start and try again
+			}
+
+			// // Step 2.5: Clean up temp files and kill other running scripts to speed up the reload cycle
+			// if (ns.ls("home", "/Temp/").length > 0) { // Do a little clean-up to speed up save/load.
+			// 	// Step 2.5.1: Test that we aren't already kicked out of the casino before doing drastic things like killing scripts
+			// 	await setText(inputWager, `1`); // Bet just a dollar and quick the game right away, no big deal
+			// 	await click(btnStartGame);
+			// 	if (await findRetry(ns, "//p[contains(text(), 'Count:')]", true, 10)) { // If this works, we're still allowed in
+			// 		const btnStay = await findRetry(ns, "//button[text() = 'Stay']", true);
+			// 		if (btnStay) await click(btnStay); // Trigger the game to end if we didn't instantly win/lose our $1 bet.
+			// 	} else { // Otherwise, we've probably been kicked out of the casino, but...
+			// 		// because we haven't killed scripts yet, it's possible another script stole focus again. Detect and handle that case.
+			// 		if (await checkForFocusScreen()) {
+			// 			log(ns, "ERROR: It looks like something stole focus while we were trying to automate the casino. Trying again.");
+			// 			continue; // Loop back to start and try again
+			// 		}
+			// 		await ns.write(ran_flag, true, "w"); // Write a flag other scripts can check for indicating we think we've been kicked out of the casino.
+			// 		return log(ns, "INFO: We appear to already have been previously kicked out of the casino.", true);
+			// 	}
+			// 	// Step 2.5.2: Kill all other scripts if enabled (note, we assume that if the temp folder is empty, they're already killed and this is a reload)
+			// 	if (options['kill-all-scripts'])
+			// 		await killAllOtherScripts(ns, !options['no-deleting-remote-files']);
+			// 	// Step 2.5.3: Clear the temp folder on home (all transient scripts / outputs)
+			// 	await waitForProcessToComplete(ns, ns.run(getFilePath('cleanup.js')));
+			// }
+			fixMath();
+			break; // We achieved everthing we wanted, we can exit the while loop.
+		} catch (err) {
+			fixMath();
+			ns.tail(); // We're having difficulty, pop open a tail window so the user is aware.
+			log(ns, `WARNING: casino.js Caught (and suppressed) an unexpected error while navigating to blackjack. Will try again...\n` +
+				(typeof err === 'string' ? err : err.message || JSON.stringify(err)), false, 'warning');
+		}
+	}
+	return;
+}
+let doc = eval("document");
+// Some DOM helpers (partial credit to @ShamesBond)
+async function click(elem) {
+	await elem[Object.keys(elem)[1]].onClick({ isTrusted: true });
+	await _ns.sleep(1000);
+}
+async function setText(input, text) {
+	await input[Object.keys(input)[1]].onChange({ isTrusted: true, target: { value: text } });
+	await _ns.sleep(1000);
+}
+function find(xpath) { return doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; }
+async function findRetry(ns, xpath, expectFailure = false, retries = null) {
+	try {
+		return await autoRetry(ns, () => find(xpath), e => e !== undefined,
+			() => expectFailure ? `It's looking like the element with xpath: ${xpath} isn't present...` :
+				`Could not find the element with xpath: ${xpath}\nSomething may have re-routed the UI`,
+			retries != null ? retries : expectFailure ? 3 : 10, 1, 2);
+	} catch (e) {
+		if (!expectFailure) throw e;
+	}
+}
+/** Helper to open a tail window and log a message to the console and terminal. Useful when trying to inform the player of a failure.
+ * @param {NS} ns **/
+ function tailAndLog(ns, message) {
+	ns.tail();
+	log(ns, message, true);
 }
--- hack/utils.js
+++ hack/utils.js
@@ -70,3 +70,127 @@ export function launchScriptHelper(ns, baseScriptName, args = [], convertFileNam
 	return pid;
 }
 
+/** Helper to retry something that failed temporarily (can happen when e.g. we temporarily don't have enough RAM to run)
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point */
+ export async function autoRetry(ns, fnFunctionThatMayFail, fnSuccessCondition, errorContext = "Success condition not met",
+ maxRetries = 5, initialRetryDelayMs = 50, backoffRate = 3, verbose = false, tprintFatalErrors = true) {
+ checkNsInstance(ns, '"autoRetry"');
+ let retryDelayMs = initialRetryDelayMs, attempts = 0;
+ while (attempts++ <= maxRetries) {
+     try {
+         const result = await fnFunctionThatMayFail()
+         const error = typeof errorContext === 'string' ? errorContext : errorContext();
+         if (!fnSuccessCondition(result))
+             throw asError(error);
+         return result;
+     }
+     catch (error) {
+         const fatal = attempts >= maxRetries;
+         log(ns, `${fatal ? 'FAIL' : 'INFO'}: Attempt ${attempts} of ${maxRetries} failed` +
+             (fatal ? `: ${typeof error === 'string' ? error : error.message || JSON.stringify(error)}` : `. Trying again in ${retryDelayMs}ms...`),
+             tprintFatalErrors && fatal, !verbose ? undefined : (fatal ? 'error' : 'info'))
+         if (fatal) throw asError(error);
+         await ns.sleep(retryDelayMs);
+         retryDelayMs *= backoffRate;
+     }
+ }
+}
+
+/** Helper to log a message, and optionally also tprint it and toast it
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point */
+ export function log(ns, message = "", alsoPrintToTerminal = false, toastStyle = "", maxToastLength = Number.MAX_SAFE_INTEGER) {
+    ns.print(message);
+    if (toastStyle) ns.toast(message.length <= maxToastLength ? message : message.substring(0, maxToastLength - 3) + "...", toastStyle);
+    if (alsoPrintToTerminal) {
+        ns.tprint(message);
+        // TODO: Find a way write things logged to the terminal to a "permanent" terminal log file, preferably without this becoming an async function.
+        //       Perhaps we copy logs to a port so that a separate script can optionally pop and append them to a file.
+        //ns.write("log.terminal.txt", message + '\n', 'a'); // Note: we should get away with not awaiting this promise since it's not a script file
+    }
+    return message;
+}
+/**
+ * Retrieve the result of an ns command by executing it in a temporary .js script, writing the result to a file, then shuting it down
+ * Importing incurs a maximum of 1.1 GB RAM (0 GB for ns.read, 1 GB for ns.run, 0.1 GB for ns.isRunning).
+ * Has the capacity to retry if there is a failure (e.g. due to lack of RAM available). Not recommended for performance-critical code.
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point
+ * @param {string} command - The ns command that should be invoked to get the desired data (e.g. "ns.getServer('home')" )
+ * @param {string=} fName - (default "/Temp/{commandhash}-data.txt") The name of the file to which data will be written to disk by a temporary process
+ * @param {args=} args - args to be passed in as arguments to command being run as a new script.
+ * @param {bool=} verbose - (default false) If set to true, pid and result of command are logged.
+ **/
+ export async function getNsDataThroughFile(ns, command, fName, args = [], verbose = false, maxRetries = 5, retryDelayMs = 50) {
+    checkNsInstance(ns, '"getNsDataThroughFile"');
+    if (!verbose) disableLogs(ns, ['run', 'isRunning']);
+    return await getNsDataThroughFile_Custom(ns, ns.run, ns.isRunning, command, fName, args, verbose, maxRetries, retryDelayMs);
+}
+
+/**
+ * An advanced version of getNsDataThroughFile that lets you pass your own "fnRun" and "fnIsAlive" implementations to reduce RAM requirements
+ * Importing incurs no RAM (now that ns.read is free) plus whatever fnRun / fnIsAlive you provide it
+ * Has the capacity to retry if there is a failure (e.g. due to lack of RAM available). Not recommended for performance-critical code.
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point
+ * @param {function} fnRun - A single-argument function used to start the new sript, e.g. `ns.run` or `(f,...args) => ns.exec(f, "home", ...args)`
+ * @param {function} fnIsAlive - A single-argument function used to start the new sript, e.g. `ns.isRunning` or `pid => ns.ps("home").some(process => process.pid === pid)`
+ * @param {args=} args - args to be passed in as arguments to command being run as a new script.
+ **/
+ export async function getNsDataThroughFile_Custom(ns, fnRun, fnIsAlive, command, fName, args = [], verbose = false, maxRetries = 5, retryDelayMs = 50) {
+    checkNsInstance(ns, '"getNsDataThroughFile_Custom"');
+    if (!verbose) disableLogs(ns, ['read']);
+    const commandHash = hashCode(command);
+    fName = fName || `/Temp/${commandHash}-data.txt`;
+    const fNameCommand = (fName || `/Temp/${commandHash}-command`) + '.js'
+    // Pre-write contents to the file that will allow us to detect if our temp script never got run
+    const initialContents = "<Insufficient RAM>";
+    await ns.write(fName, initialContents, 'w');
+    // Prepare a command that will write out a new file containing the results of the command
+    // unless it already exists with the same contents (saves time/ram to check first)
+    // If an error occurs, it will write an empty file to avoid old results being misread.
+    const commandToFile = `let r;try{r=JSON.stringify(\n` +
+        `    ${command}\n` +
+        `);}catch(e){r="ERROR: "+(typeof e=='string'?e:e.message||JSON.stringify(e));}\n` +
+        `const f="${fName}"; if(ns.read(f)!==r) await ns.write(f,r,'w')`;
+    // Run the command with auto-retries if it fails
+    const pid = await runCommand_Custom(ns, fnRun, commandToFile, fNameCommand, args, verbose, maxRetries, retryDelayMs);
+    // Wait for the process to complete (TODO: Replace the need for a `fnIsAlive` with one that simply checks the file contents are no longer `initialContents`)
+    await waitForProcessToComplete_Custom(ns, fnIsAlive, pid, verbose);
+    if (verbose) ns.print(`Process ${pid} is done. Reading the contents of ${fName}...`);
+    // Read the file, with auto-retries if it fails // TODO: Unsure reading a file can fail or needs retrying. 
+    let lastRead;
+    const fileData = await autoRetry(ns, () => ns.read(fName),
+        f => (lastRead = f) !== undefined && f !== "" && f !== initialContents && !(typeof f == "string" && f.startsWith("ERROR: ")),
+        () => `\nns.read('${fName}') returned a bad result: "${lastRead}".` +
+            `\n  Script:  ${fNameCommand}\n  Args:    ${JSON.stringify(args)}\n  Command: ${command}` +
+            (lastRead == undefined ? '\nThe developer has no idea how this could have happened. Please post a screenshot of this error on discord.' :
+                lastRead == initialContents ? `\nThe script that ran this will likely recover and try again later once you have more free ram.` :
+                    lastRead == "" ? `\nThe file appears to have been deleted before a result could be retrieved. Perhaps there is a conflicting script.` :
+                        `\nThe script was likely passed invalid arguments. Please post a screenshot of this error on discord.`),
+        maxRetries, retryDelayMs, undefined, verbose, verbose);
+    if (verbose) ns.print(`Read the following data for command ${command}:\n${fileData}`);
+    return JSON.parse(fileData); // Deserialize it back into an object/array and return
+}
+/**
+ * An advanced version of waitForProcessToComplete that lets you pass your own "isAlive" test to reduce RAM requirements (e.g. to avoid referencing ns.isRunning)
+ * Importing incurs 0 GB RAM (assuming fnIsAlive is implemented using another ns function you already reference elsewhere like ns.ps) 
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point
+ * @param {function} fnIsAlive - A single-argument function used to start the new sript, e.g. `ns.isRunning` or `pid => ns.ps("home").some(process => process.pid === pid)`
+ **/
+ export async function waitForProcessToComplete_Custom(ns, fnIsAlive, pid, verbose) {
+    checkNsInstance(ns, '"waitForProcessToComplete_Custom"');
+    if (!verbose) disableLogs(ns, ['sleep']);
+    // Wait for the PID to stop running (cheaper than e.g. deleting (rm) a possibly pre-existing file and waiting for it to be recreated)
+    let start = Date.now();
+    let sleepMs = 1;
+    for (var retries = 0; retries < 1000; retries++) {
+        if (!fnIsAlive(pid)) break; // Script is done running
+        if (verbose && retries % 100 === 0) ns.print(`Waiting for pid ${pid} to complete... (${formatDuration(Date.now() - start)})`);
+        await ns.sleep(sleepMs);
+        sleepMs = Math.min(sleepMs * 2, 200);
+    }
+    // Make sure that the process has shut down and we haven't just stopped retrying
+    if (fnIsAlive(pid)) {
+        let errorMessage = `run-command pid ${pid} is running much longer than expected. Max retries exceeded.`;
+        ns.print(errorMessage);
+        throw new Error(errorMessage);
+    }
+}
